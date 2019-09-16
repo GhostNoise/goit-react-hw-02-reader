6461b395ddbcdd1b5f90aeaa91e7079806059e1a
Vlad Shum <shumanskyivlad@gmail.com>
2019-09-16 17:14:11 +0300

    Updates

---
+++ precache-manifest.30ca8e4daecf925bbf7ef6d10915ba22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0f8f2e436fc577230d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.681d89f7.js"
+  },
+  {
+    "revision": "0942166d7ad4f6cecb63",
+    "url": "/goit-react-hw-01-components/static/js/main.37393dae.chunk.js"
+  },
+  {
+    "revision": "5d1ebc32fb86ed48fab4",
+    "url": "/goit-react-hw-01-components/static/js/2.bb14b3da.chunk.js"
+  },
+  {
+    "revision": "0942166d7ad4f6cecb63",
+    "url": "/goit-react-hw-01-components/static/css/main.e6cf9534.chunk.css"
+  },
+  {
+    "revision": "58c54d944368bfd40c4dfa523f2d439b",
+    "url": "/goit-react-hw-01-components/index.html"
+  }
+];
